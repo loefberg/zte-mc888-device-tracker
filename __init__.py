4b8b4176d03658c0ab393ffce134d6a6c8c446cd
Richard Löfberg <richard@2c8.com>
2023-12-17 00:21:21 +0100

    Added setup()

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-"""The tomato component."""
+"""The zte component."""
+
+DOMAIN = "zte"
+
+
+def setup(hass, config):
+    # Return boolean to indicate that initialization was successful.
+    return True
